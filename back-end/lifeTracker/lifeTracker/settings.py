6ce61c7fce7046f9002a8652d33c9a781d1534f8
jessicant <jhamilton159@outlook.com>
2022-03-08 17:34:24 -0800

    Organizing files in a logical way

--- back-end/lifeTracker/lifeTracker/settings.py
+++ back-end/lifeTracker/lifeTracker/settings.py
@@ -18,13 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 with open('securityKey.txt') as f:
-    SECURITY_KEY = f.readline()
-
-
-
+    SECRET_KEY = f.readline()
+    
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
